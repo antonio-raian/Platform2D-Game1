aa5005a7c3706d25228123d84c646a0235924dc6
Antonio pc DevoluTI <raymendesjr2013@gmail.com>
2021-06-10 12:07:59 -0300

    add: Singleton Global

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+export var fruits = 1
+
 func _on_item_body_entered(body):
 	$animation.play("collected")
+	Global.fruits += fruits
 
 func _on_animation_animation_finished(anim_name):
 	if(anim_name=="collected"):
